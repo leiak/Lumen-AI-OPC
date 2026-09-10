2e850f5e333e28e9ab45270984faaca8f72135be
fang <fang@chaonao.com>
2026-09-10 21:46:20 +0800

    fix(notification): align pom with opc-billing pattern + add @EnableCustomConfig (Task 1 fix)

--- springboot3/ruoyi-modules/opc-notification/src/main/java/com/ruoyi/opc/notification/OpcNotificationApplication.java
+++ springboot3/ruoyi-modules/opc-notification/src/main/java/com/ruoyi/opc/notification/OpcNotificationApplication.java
@@ -1,15 +1,24 @@
 package com.ruoyi.opc.notification;
 
+import com.ruoyi.common.security.annotation.EnableCustomConfig;
+import com.ruoyi.common.security.annotation.EnableRyFeignClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import com.ruoyi.common.security.annotation.EnableRyFeignClients;
 
+/**
+ * OPC 通知中心启动类（邮件 / 短信 / 站内信 / WebSocket）
+ *
+ * @author OAC
+ */
+@EnableCustomConfig
+@EnableRyFeignClients(basePackages = { "com.ruoyi.system.api", "com.ruoyi.opc" })
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.ruoyi.system", "com.ruoyi.opc"})
-@EnableRyFeignClients(basePackages = {"com.ruoyi.system.api", "com.ruoyi.opc"})
+@ComponentScan({ "com.ruoyi.opc", "com.ruoyi.system" })
 public class OpcNotificationApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(OpcNotificationApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  OPC 通知中心启动成功   ლ(´ڡ`ლ)ﾞ");
     }
 }
